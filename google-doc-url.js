2c92d0fa8e8b61f8ea78f6877f9426c762dfb329
Bobby P <bobby.palta@pipelinenow.com>
2018-12-18 00:12:13 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Wrvm-t1LVZywF1h1i6d4y_1ozlraCBrE9V_cWJIH_Ww/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Wrvm-t1LVZywF1h1i6d4y_1ozlraCBrE9V_cWJIH_Ww/edit#gid=0';
